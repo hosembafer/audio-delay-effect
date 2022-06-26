850c6090284a69606a72a6243579f51b39f3f1cc
Rafael Hovhannisyan <hovhannisian.rafael@gmail.com>
2022-06-26 18:22:07 +0400

    Update README.md

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # audio-delay-effect
 
 Audio Delay linear effect compatible with Web Audio API
+
+## Effects
+- Delay
+
+## Build and Run
+```shell
+
+sh try.sh
+```
--- main.cpp
+++ main.cpp
@@ -10,10 +10,10 @@ int main()
     std::filesystem::create_directory("results");
     
 
-    delay("./assets/punch-double-stereo.wav", "./results/punch-result-delay-stereo.wav", delayTime, feedback, dryWet);
-    delay("./assets/punch-double-mono.wav", "./results/punch-result-delay-mono.wav", delayTime, feedback, dryWet);
+    delay("./samples/punch-double-stereo.wav", "./results/punch-result-delay-stereo.wav", delayTime, feedback, dryWet);
+    delay("./samples/punch-double-mono.wav", "./results/punch-result-delay-mono.wav", delayTime, feedback, dryWet);
 
-    delay("./assets/2-speakers-1.53-minutes.wav", "./results/speak-result-delay-stereo.wav", delayTime, feedback, dryWet);
+    delay("./samples/2-speakers-1.53-minutes.wav", "./results/speak-result-delay-stereo.wav", delayTime, feedback, dryWet);
 
     return 0;
 }
