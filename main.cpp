6fbc928f909065ba60ed8ee81fb2f858903b32c6
Rafael Hovhannisyan <hovhannisian.rafael@gmail.com>
2022-06-27 08:13:49 +0400

    feat: add log bool

--- main.cpp
+++ main.cpp
@@ -6,14 +6,15 @@ int main()
     int delayTime = 250;
     int feedback = 50;
     int dryWet = 50;
+    bool log = true;
 
     std::filesystem::create_directory("results");
     
 
-    delay("./samples/punch-double-stereo.wav", "./results/punch-result-delay-stereo.wav", delayTime, feedback, dryWet);
-    delay("./samples/punch-double-mono.wav", "./results/punch-result-delay-mono.wav", delayTime, feedback, dryWet);
+    delay("./samples/punch-double-stereo.wav", "./results/punch-result-delay-stereo.wav", delayTime, feedback, dryWet, log);
+    delay("./samples/punch-double-mono.wav", "./results/punch-result-delay-mono.wav", delayTime, feedback, dryWet, log);
 
-    delay("./samples/2-speakers-1.53-minutes.wav", "./results/speak-result-delay-stereo.wav", delayTime, feedback, dryWet);
+    delay("./samples/2-speakers-1.53-minutes.wav", "./results/speak-result-delay-stereo.wav", delayTime, feedback, dryWet, log);
 
     return 0;
 }
